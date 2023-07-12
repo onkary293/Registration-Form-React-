845778e1a2530f01eb13c8c94469a88ab6ae2424
Yadav Onkar Abasaheb <91131634+onkary293@users.noreply.github.com>
2023-07-12 17:44:37 +0530

    my First commit login from react website

---
+++ src/components/Forms.js
@@ -0,0 +1,61 @@
+import {React,useState} from 'react'
+function Forms ()
+{
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [allEntry, setAllEntry] = useState([]);
+    const clickHandler = (e) =>
+    {
+        e.preventDefault();
+        const newEntry = {email: email, password: password}
+        setAllEntry([...allEntry, newEntry])
+        console.log(allEntry)
+        setEmail("");
+        setPassword("");
+       
+    }
+    return (
+      <div className="Formcss">
+        <h1>Registration</h1>
+        <div className="form-style">
+          <form action="" className="formcss" onSubmit={clickHandler}>
+            <fieldset className="formcss">
+              <input
+                type="email"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+                value={email}
+                placeholder="Enter Your Email"
+                required
+              />
+              <input
+                type="password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+                value={password}
+                placeholder="Enter Your Password"
+                required
+              />
+              <button type="submit">Submit</button>
+            </fieldset>
+          </form>
+        </div>
+        <div>
+          <h2>Users Submitted Form List :</h2>
+          {allEntry.map((curElem) => {
+            return (
+              <div className="List">
+                <p>
+                  Email: {curElem.email} Password: {curElem.password}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+}
+
+export default Forms;
